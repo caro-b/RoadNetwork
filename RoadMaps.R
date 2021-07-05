4bb082f246dd299a377e11e9d46f460ba6e3f5d6
caro-b <caroline.anton@web.de>
2021-07-05 18:58:40 +0200

    automate input only osm roads & add. IZW roads

--- RoadMaps.R
+++ RoadMaps.R
@@ -11,6 +11,7 @@ if(length(new.packages)) install.packages(new.packages)
 lapply(packagelist, require, character.only = TRUE)
 
 
+
 #### DATA IMPORT ####
 getwd()
 
@@ -19,7 +20,7 @@ roads_2014 <- readOGR("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/2014.shp")
 aoi <- readOGR("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/prediction_area.shp")
 
 
-## OSM - roads data
+## input OSM - roads data
 
 # convert bb to an overpass query object (API)
 roads_osm <- opq(aoi@bbox) %>%
@@ -31,55 +32,29 @@ roads_osm <- opq(aoi@bbox) %>%
 roads_osm_lines <- roads_osm$osm_lines
 
 
-## GEE - Hansen Global Forest Change
+## input Hansen GFC raster files 
 
-# needs prior python installation
-# # It is necessary just once
-# ee_install()
-# 
-# # Initialize Earth Engine!
-# ee_Initialize()
+# set directory where downloaded files from GEE are in
+dir <- "C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/"
 
-# aoi_sf <- st_as_sf(aoi)
-# aoi_sf <- st_set_crs(aoi_sf, 4326)
-# 
-# ee_aoi <- aoi_sf %>% 
-#  # st_read(quiet = TRUE) %>% 
-#   sf_as_ee()
-# region <- mask$geometry()$bounds()
-# 
-# ee_gfc <- ee$
-#   Image('UMD/hansen/global_forest_change_2020_v1_8')$
-#   clip(ee_aoi)
-# 
-# gfc <- ee_as_raster(ee_gfc, maxPixels=200000000)
-#plot(gfc)
+# read all files from directory into list
+files <- list.files(path= dir, pattern="*.tif", full.names=T)
 
-gfc_01 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_01.tif")
-gfc_02 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_02.tif")
-gfc_03 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_03.tif")
-gfc_04 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_04.tif")
-gfc_05 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_05.tif")
-gfc_06 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_06.tif")
-gfc_07 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_07.tif")
-gfc_08 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_08.tif")
-gfc_09 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_09.tif")
-gfc_10 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_10.tif")
-gfc_11 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_11.tif")
-gfc_12 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_12.tif")
-gfc_13 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_13.tif")
-gfc_14 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_14.tif")
-gfc_15 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_15.tif")
-gfc_16 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_16.tif")
-gfc_17 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_17.tif")
-gfc_18 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_18.tif")
-gfc_19 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_19.tif")
-gfc_20 <- raster("C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/forest_loss_20.tif")
-#dir <- list.files(path="C:/Users/carob/Dropbox/EAGLE/SS21/Conservation", pattern="*.tif")
-#for (i in 1:length(dir)) assign(dir[i], raster(dir[i]))
+# stack raster images
+gfc_stack <- stack(files)
+
+# convert to raster brick to shorten processing time
+gfc <- brick(gfc_stack)
 
 
-#### ANALYSIS ####
+
+
+#### PRE-PROCESSING ####
+
+# set crs of aoi
+crs(aoi) <- crs(gfc$forest_loss_01)
+crs(roads_osm_lines) <- crs(gfc$forest_loss_01)
+crs(roads_2014) <- crs(gfc$forest_loss_01)
 
 # clip roads to aoi 
 # byid = T to keep single features (roads)
@@ -93,164 +68,219 @@ plot(roads_2014_clip, add = T, col ="blue")
 plot(roads_osm_clip, add = T, col ="orange")
 legend("topleft",legend=c("Roads IZW (2014)","Roads OSM (2020)"),
        col=c("blue","orange"),lty = 1)
+# take OSM data (as better for scaling)
 
 
-# union roads from OSM & IZW
-roads_union <- union(roads_osm_clip, roads_2014_clip)
-crs(roads_union) <- crs(gfc_01)
+# combine roads from OSM & IZW
 
-## mask loss areas in gfc data
+# remove roads where OSM & IZW roads intersect (only include additional roads from IZW which aren't in OSM data)
+roads_2014_new <- roads_2014_clip[is.na(over(roads_2014_clip,roads_osm_clip))]
 
-# function for function for masking loss areas in gfc loss data according to road data
-maskForestLoss <- function(forest_loss, roads_union) {
+# only use osm roads, add additional IZW roads
+roads_union <- rbind(roads_osm_clip,roads_2014_new)
+
+
+
+## buffer around roads (as OSM only provides lines, not the original street width)
+# highway width = 22m
+# crs needs to be in long/lat for buffer in meters
+roads_union@proj4string #WGS84 (EPSG: 4326)
+
+# set crs to EPSG:3405 VN-2000 / UTM zone 48N
+# https://spatialreference.org/ref/epsg/vn-2000-utm-zone-48n/
+crs(roads_union) <- CRS("+proj=utm +zone=48 +ellps=WGS84 +units=m +no_defs")
+
+roads_union_buffer <- buffer(roads_union, width = 0.0005, dissolve=F) #~50m buffer
+
+plot(roads_union)
+plot(roads_union_buffer, add=T)
+
+
+
+#### ANALYSIS ####
+
+## function for masking forest loss areas in gfc loss data according to road data
+maskForestLoss <- function(forest_loss, roads) {
   # masking forest loss areas in gfc loss data according to road data
   forest_loss[forest_loss < 1] <- NA
-  forest_loss_roads <- mask(forest_loss, roads_union)
+  forest_loss_roads <- mask(forest_loss, roads)
   # raster to polygons
-  forest_loss_roads_pol <- rasterToPolygons(forest_loss_roads, dissolve=T)
+  forest_loss_roads_pol <- rasterToPolygons(forest_loss_roads, dissolve=F)
   return(forest_loss_roads_pol)
 }
 
 
+#### question for Martin: why does area(gfc_01_roads_pol) correspond to pixel size (900m²), but width oly works with degrees?? ####
+
+# function to make buffer around forest loss pixels to check if forest loss pixels actually correspond to road development
+bufferPixels <- function(gfc_roads_pol) {
+  # buffering for SpatialPolygons can only deal with planar coordinate reference systems (eg UTM)
+  gfc_buffer <- buffer(gfc_roads_pol, width = 0.001, dissolve=F) # ~100m buffer around forest loss pixels
+  # check if more than x forest loss polygons in buffer - to only include forest loss polygons which correspond to road development
+  # as each polygon corresponds to one pixel (and they have the similar size), possible to sum up pixels
+  # count number of polygons in per buffer
+  polygonsInBuffer <- over(gfc_roads_pol, gfc_buffer, fn = sum)
+  #sort(test$forest_loss_01)
+  # remove polygons which don't have 3 or more forest loss pixels per buffer 
+  # (1 pixel = ~30m x 30m = 900m² forest loss), (3 pixels =~ 1800m² = 1.8km)
+  # buffer (100m) / 3 * 30m
+  roadDevPixels <- gfc_roads_pol[polygonsInBuffer$forest_loss_01 > 2,]
+  return(roadDevPixels)
+}
+#### TODO: altern. with area instead of sum ? ####
+
+
 ## year 2001
-# mask loss areas in gfc loss data according to road data
-gfc_01_roads_pol <- maskForestLoss(gfc_01, roads_union)
+# mask loss areas in gfc loss data according to road data (using function)
+gfc_01_roads_pol <- maskForestLoss(gfc$forest_loss_01, roads_union_buffer)
+
+plot(gfc_01_roads_pol)
+plot(gfc_01_buffer, add = T)
+
+# function to buffer around forest loss pixels to get forest loss pixels corresponding to road development
+gfc_01_roadDev <- bufferPixels(gfc_01_roads_pol)
 
 # roads which intersect with forest loss
-intersect_01 <- roads_union[intersect(roads_union, gfc_01_roads_pol)]
+intersect_01 <- roads_union_buffer[intersect(roads_union_buffer, gfc_01_roadDev)]
 
 # remove intersecting roads from road network for analysis of next year
-roads_union_01 <- roads_union[is.na(over(roads_union, gfc_01_roads_pol))]
+roads_union_01 <- roads_union_buffer[is.na(over(roads_union_buffer, gfc_01_roadDev))]
+
+
+#### TODO: subdivide roads into ~km (?) segments??? ####
+# length of spatial lines
+sort(gLength(roads_union, byid = T))
+
 
 
 ## year 2002
-gfc_02_roads_pol <- maskForestLoss(gfc_02, roads_union)
+gfc_02_roads_pol <- maskForestLoss(gfc$forest_loss_02, roads_union)
 
 intersect_02 <- roads_union_01[intersect(roads_union_01, gfc_02_roads_pol)]
 roads_union_02 <- roads_union_01[is.na(over(roads_union_01, gfc_02_roads_pol))]
 
 
 ## year 2003
-gfc_03_roads_pol <- maskForestLoss(gfc_03, roads_union)
+gfc_03_roads_pol <- maskForestLoss(gfc$forest_loss_03, roads_union)
 
 intersect_03 <- roads_union_02[intersect(roads_union_02, gfc_03_roads_pol)]
 roads_union_03 <- roads_union_02[is.na(over(roads_union_02, gfc_03_roads_pol))]
 
 
 ## year 2004
-gfc_04_roads_pol <- maskForestLoss(gfc_04, roads_union)
+gfc_04_roads_pol <- maskForestLoss(gfc$forest_loss_04, roads_union)
 
 intersect_04 <- roads_union_03[intersect(roads_union_03, gfc_04_roads_pol)]
 roads_union_04 <- roads_union_03[is.na(over(roads_union_03, gfc_04_roads_pol))]
 
 
 ## year 2005
-gfc_05_roads_pol <- maskForestLoss(gfc_05, roads_union)
+gfc_05_roads_pol <- maskForestLoss(gfc$forest_loss_05, roads_union)
 
 intersect_05 <- roads_union_04[intersect(roads_union_04, gfc_05_roads_pol)]
 roads_union_05 <- roads_union_04[is.na(over(roads_union_04, gfc_05_roads_pol))]
 
 
 ## year 2006
-gfc_06_roads_pol <- maskForestLoss(gfc_06, roads_union)
+gfc_06_roads_pol <- maskForestLoss(gfc$forest_loss_06, roads_union)
 
 intersect_06 <- roads_union_05[intersect(roads_union_05, gfc_06_roads_pol)] # no intersecting roads
 roads_union_06 <- roads_union_05[is.na(over(roads_union_05, gfc_06_roads_pol))]
 
 
 ## year 2007
-gfc_07_roads_pol <- maskForestLoss(gfc_07, roads_union)
+gfc_07_roads_pol <- maskForestLoss(gfc$forest_loss_07, roads_union)
 
 intersect_07 <- roads_union_06[intersect(roads_union_06, gfc_07_roads_pol)]
 roads_union_07 <- roads_union_06[is.na(over(roads_union_06, gfc_07_roads_pol))]
 
 
 ## year 2008
-gfc_08_roads_pol <- maskForestLoss(gfc_08, roads_union)
+gfc_08_roads_pol <- maskForestLoss(gfc$forest_loss_08, roads_union)
 
 intersect_08 <- roads_union_07[intersect(roads_union_07, gfc_08_roads_pol)]
 roads_union_08 <- roads_union_07[is.na(over(roads_union_07, gfc_08_roads_pol))]
 
 
 ## year 2009
-gfc_09_roads_pol <- maskForestLoss(gfc_09, roads_union)
+gfc_09_roads_pol <- maskForestLoss(gfc$forest_loss_09, roads_union)
 
 intersect_09 <- roads_union_08[intersect(roads_union_08, gfc_09_roads_pol)]
 roads_union_09 <- roads_union_08[is.na(over(roads_union_08, gfc_09_roads_pol))]
 
 
 ## year 2010
-gfc_10_roads_pol <- maskForestLoss(gfc_10, roads_union)
+gfc_10_roads_pol <- maskForestLoss(gfc$forest_loss_10, roads_union)
 
 intersect_10 <- roads_union_09[intersect(roads_union_09, gfc_10_roads_pol)] # no intersecting roads
 roads_union_10 <- roads_union_09[is.na(over(roads_union_09, gfc_10_roads_pol))]
 
 
 ## year 2011
-gfc_11_roads_pol <- maskForestLoss(gfc_11, roads_union)
+gfc_11_roads_pol <- maskForestLoss(gfc$forest_loss_11, roads_union)
 
 intersect_11 <- roads_union_10[intersect(roads_union_10, gfc_11_roads_pol)]
 roads_union_11 <- roads_union_10[is.na(over(roads_union_10, gfc_11_roads_pol))]
 
 
 ## year 2012
-gfc_12_roads_pol <- maskForestLoss(gfc_12, roads_union)
+gfc_12_roads_pol <- maskForestLoss(gfc$forest_loss_12, roads_union)
 
 intersect_12 <- roads_union_11[intersect(roads_union_11, gfc_12_roads_pol)]
 roads_union_12 <- roads_union_11[is.na(over(roads_union_11, gfc_12_roads_pol))]
 
 
 ## year 2013
-gfc_13_roads_pol <- maskForestLoss(gfc_13, roads_union)
+gfc_13_roads_pol <- maskForestLoss(gfc$forest_loss_13, roads_union)
 
 intersect_13 <- roads_union_12[intersect(roads_union_12, gfc_13_roads_pol)]
 roads_union_13 <- roads_union_12[is.na(over(roads_union_12, gfc_13_roads_pol))]
 
 
 ## year 2014
-gfc_14_roads_pol <- maskForestLoss(gfc_14, roads_union)
+gfc_14_roads_pol <- maskForestLoss(gfc$forest_loss_14, roads_union)
 
 intersect_14 <- roads_union_13[intersect(roads_union_13, gfc_14_roads_pol)]
 roads_union_14 <- roads_union_13[is.na(over(roads_union_13, gfc_14_roads_pol))]
 
 
 ## year 2015
-gfc_15_roads_pol <- maskForestLoss(gfc_15, roads_union)
+gfc_15_roads_pol <- maskForestLoss(gfc$forest_loss_15, roads_union)
 
 intersect_15 <- roads_union_14[intersect(roads_union_14, gfc_15_roads_pol)]
 roads_union_15 <- roads_union_14[is.na(over(roads_union_14, gfc_15_roads_pol))]
 
 
 ## year 2016
-gfc_16_roads_pol <- maskForestLoss(gfc_16, roads_union)
+gfc_16_roads_pol <- maskForestLoss(gfc$forest_loss_16, roads_union)
 
 intersect_16 <- roads_union_15[intersect(roads_union_15, gfc_16_roads_pol)]
 roads_union_16 <- roads_union_15[is.na(over(roads_union_15, gfc_16_roads_pol))]
 
 
 ## year 2017
-gfc_17_roads_pol <- maskForestLoss(gfc_17, roads_union)
+gfc_17_roads_pol <- maskForestLoss(gfc$forest_loss_17, roads_union)
 
 intersect_17 <- roads_union_16[intersect(roads_union_16, gfc_17_roads_pol)] # no intersecting roads
 roads_union_17 <- roads_union_16[is.na(over(roads_union_16, gfc_17_roads_pol))]
 
 
 ## year 2018
-gfc_18_roads_pol <- maskForestLoss(gfc_18, roads_union)
+gfc_18_roads_pol <- maskForestLoss(gfc$forest_loss_18, roads_union)
 
 intersect_18 <- roads_union_17[intersect(roads_union_17, gfc_18_roads_pol)]
 roads_union_18 <- roads_union_17[is.na(over(roads_union_17, gfc_18_roads_pol))]
 
 
 ## year 2019
-gfc_19_roads_pol <- maskForestLoss(gfc_19, roads_union)
+gfc_19_roads_pol <- maskForestLoss(gfc$forest_loss_19, roads_union)
 
 intersect_19 <- roads_union_18[intersect(roads_union_18, gfc_19_roads_pol)]
 roads_union_19 <- roads_union_18[is.na(over(roads_union_18, gfc_19_roads_pol))]
 
 
 ## year 2020
-gfc_20_roads_pol <- maskForestLoss(gfc_20, roads_union)
+gfc_20_roads_pol <- maskForestLoss(gfc$forest_loss_20, roads_union)
 
 intersect_20 <- roads_union_19[intersect(roads_union_19, gfc_20_roads_pol)] # no intersecting roads
 roads_union_20 <- roads_union_19[is.na(over(roads_union_19, gfc_20_roads_pol))]
@@ -376,5 +406,5 @@ plot(area)
   transition_states(year) +
   ease_aes('linear') 
 
-# rather consecutive road development of previous years?
+## rather consecutive road development of previous years
 
