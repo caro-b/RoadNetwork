d7ce8f5dfc7514036f5012e15c0339e3c010489a
caro-b <caroline.anton@web.de>
2021-07-09 15:21:10 +0200

    updated naming data export

--- RoadMaps.R
+++ RoadMaps.R
@@ -1,251 +1,231 @@
 
 #### SETUP ####
 
-## Packages
-# install required packages if needed
+## install required packages (if not installed yet)
 packagelist <- c("dplyr","gganimate","ggplot2","ggthemes","maps","osmdata","raster","rgee","rgdal","rgeos","sf","tidyverse")
 new.packages <- packagelist[!(packagelist %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
-# Load required packages
+## load required packages
 lapply(packagelist, require, character.only = TRUE)
 
 
 
 #### DATA IMPORT ####
+
+## get working directory
 getwd()
 
-# set directory to folder where input files lie
-dir <- "C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/"
+## set directory to folder where input files lie
+dir <- "C:/Users/carob/Dropbox/EAGLE/SS21/Conservation/RoadNetwork/"
 
-# input area of interest
+## input area of interest (Bach Ma Nationalpark)
 aoi <- readOGR(paste(dir, "prediction_area.shp", sep = ""))
 
-# input manually digitized roads from IZW
-roads_2014 <- readOGR(paste(dir, "2014.shp", sep = ""))
+## input manually digitized roads from IZW
+roads_IZW_2014 <- readOGR(paste(dir, "2014.shp", sep = ""))
 
-
-## input OSM - roads data
-
-# convert bb to an overpass query object (API)
+## input OSM road data
+# convert bounding box to an overpass query object (API)
 roads_osm <- opq(aoi@bbox) %>%
+  # get roads from OSM
   add_osm_feature(key = "highway") %>%  
-  # output as simple features object (or R spatial (sp) - osmdata_sp()) - advantage of sf: use geom_sf() for ggplot2
+  # output as spatial object (sp)
   osmdata_sp()
 
-# extract roads as lines
+## extract roads as line objects
 roads_osm_lines <- roads_osm$osm_lines
 
 
 ## input Hansen GFC raster files 
+# read all forest_loss files from directory into list
+gfc_files <- list.files(path = dir, pattern = "forest_loss", full.names=T)
 
-# read all files from directory into list
-files <- list.files(path= dir, pattern="*.tif", full.names=T)
-
-# stack raster images
-gfc_stack <- stack(files)
+# stack raster layers
+gfc_stack <- stack(gfc_files)
 
 # convert to raster brick to shorten processing time
 gfc <- brick(gfc_stack)
 
 
 
-
 #### PRE-PROCESSING ####
 
-crs(gfc$forest_loss_01)
-# CRS("+proj=longlat +datum=WGS84")
-
-# set crs of aoi
-crs(aoi) <- crs(gfc$forest_loss_01)
+## set to CRS of GFC data
+# set CRS of aoi 
+crs(gfc[[1]]) # CRS("+proj=longlat +datum=WGS84")
+crs(aoi) <- crs(gfc[[1]])
 
 # transform crs of roads
 roads_osm_lines <- spTransform(roads_osm_lines, CRS("+proj=longlat +datum=WGS84"))
-crs(roads_2014) <- crs(gfc$forest_loss_01)
+crs(roads_IZW_2014) <- crs(gfc[[1]])
 
-# clip roads to aoi 
+## clip roads to aoi 
 # byid = T to keep single features (roads)
 roads_osm_clip <- gIntersection(roads_osm_lines, aoi, byid = T)
-roads_2014_clip <- gIntersection(roads_2014, aoi, byid = T)
+roads_IZW_2014_clip <- gIntersection(roads_IZW_2014, aoi, byid = T)
 
-
-# discrepancies between OSM & IZW road data
+## plot discrepancies between OSM & IZW road data
 plot(aoi)
-plot(roads_2014_clip, add = T, col ="blue")
-plot(roads_osm_clip, add = T, col ="orange")
-legend("topleft",legend=c("Roads IZW (2014)","Roads OSM (2020)"),
-       col=c("blue","orange"),lty = 1)
+plot(roads_IZW_2014_clip, add = T, col = "blue")
+plot(roads_osm_clip, add = T, col = "orange")
+legend("topleft", legend = c("Roads IZW (2014)", "Roads OSM (2020)"),
+       col = c("blue", "orange"), lty = 1)
 
-# combine roads from OSM & IZW
-
+## combine roads from OSM & IZW
+# take OSM data (as better for scaling to other areas)
 # remove roads where OSM & IZW roads intersect
-# take OSM data (as better for scaling)
-# (only include additional roads from IZW which aren't in OSM data)
-roads_2014_new <- roads_2014_clip[is.na(over(roads_2014_clip,roads_osm_clip))]
+roads_IZW_2014_new <- roads_IZW_2014_clip[is.na(over(roads_IZW_2014_clip, roads_osm_clip))]
 
-# only use osm roads, add additional IZW roads
-roads_union <- rbind(roads_osm_clip, roads_2014_new)
+# only use osm roads, add additional IZW roads which aren't in OSM data
+roads_union <- rbind(roads_osm_clip, roads_IZW_2014_new)
 
-
-
-## buffer around roads (as OSM only provides lines, not the original street width)
+## buffer around roads (as OSM only provides lines, not the original road width)
 # highway width = 22m
 # highway expected to be opened in 2021
-# crs needs to be in long/lat for buffer in meters
-roads_union@proj4string # WGS84 (EPSG: 4326)
+# source: https://vinlove.net/2021/04/05/expressway-through-bach-ma-national-park-before-traffic-opening-date/
+roads_union_buffer <- buffer(roads_union, width = 0.0001, dissolve = F) # ~10m buffer
 
-# set crs to EPSG:3405 VN-2000 / UTM zone 48N
-# https://spatialreference.org/ref/epsg/vn-2000-utm-zone-48n/
-# Proj4js.defs["EPSG:3405"] = "+proj=utm +zone=48 +ellps=WGS84 +units=m +no_defs";
-#roads_union <- spTransform(roads_union, CRS("+proj=utm +zone=48 +ellps=WGS84 +units=m +no_defs"))
-
-roads_union_buffer <- buffer(roads_union, width = 0.0001, dissolve=F) #~10m buffer: 0.0001
-
-plot(roads_union)
-plot(roads_union_buffer, add=T)
-
-#### question for Martin: why does area(gfc_01_roads_pol) correspond to pixel size (900m²), but width in buffer() only works with degrees?? ####
-## buffer() - Unit of width is meter if x has a longitude/latitude CRS
 
 
 #### ANALYSIS ####
 
-## function for masking forest loss areas in gfc loss data according to road data
+## function for masking forest loss areas in gfc data according to road data
 maskForestLoss <- function(forest_loss, roads) {
   # masking forest loss areas in gfc loss data according to road data
   forest_loss[forest_loss < 1] <- NA
   forest_loss_roads <- mask(forest_loss, roads)
-  forest_loss_roads_pol <- rasterToPolygons(forest_loss_roads, dissolve=F)
+  forest_loss_roads_pol <- rasterToPolygons(forest_loss_roads, dissolve = F)
   return(forest_loss_roads_pol)
 }
 
 
-## iterate over all raster layers (=years): 
-# 1. mask forest loss according to roads 
-# 2. polygonize raster pixels
-# 3. save polygons into one spatialpolygonsdataframe & add year (as numeric)
+## Procedure: iterate over all raster layers (layers correspond to years): 
+## 1. mask forest loss according to roads 
+## 2. polygonize forest loss pixels
+## 3. save polygons into one spatialpolygonsdataframe & add year column
 
 # index for iteration: starts at year 2001 (no forest loss in year 2000) = second layer in raster
-d <- as.numeric(1)
+d <- 1
 
-# create empty spatialpolygons object
-pol<- SpatialPolygons(list(), proj4string=crs(gfc$forest_loss_01))
-dat <- data.frame(matrix(ncol=1,nrow=0, dimnames=list(NULL, c("year"))))
-sp <- SpatialPolygonsDataFrame(pol, data= dat)
+# create empty spatialpolygonsdataframe with year column
+polygons_empty <- SpatialPolygons(list(), proj4string = crs(gfc[[1]]))
+data_empty <- data.frame(matrix(ncol = 1, nrow = 0, dimnames = list(NULL, c("year"))))
+sp_forestloss <- SpatialPolygonsDataFrame(polygons_empty, data = data_empty)
 
+# iterate over rasterstack layers (years)
 while(d < (nlayers(gfc))) {  
-  # mask forest loss according to roads & polygonize raster pixels via function maskForestLoss
-  # save as spatialpolygons (remove data section which would lead to errors in rbind due to different column naming)
-  sp_new <- SpatialPolygons(maskForestLoss(gfc[[d+1]], roads_union_buffer)@polygons, proj4string=crs(gfc$forest_loss_01))
-  # add year as column
-  sp_new$year <- rep(d, length(sp_new))
-  # save all polygons into one spatialpolygonsdataframe (via rbind())
-  sp <- rbind(sp, sp_new)
-  d=d+1
+  # mask forest loss according to roads & polygonize raster pixels via function maskForestLoss()
+  # save as spatialpolygons
+  sp_forestloss_new <- SpatialPolygons(maskForestLoss(gfc[[d+1]], roads_union_buffer)@polygons, proj4string = crs(gfc[[1]]))
+  # add year of forestloss
+  sp_forestloss_new$year <- rep(d, length(sp_forestloss_new))
+  # save all polygons into one spatialpolygonsdataframe
+  sp_forestloss <- rbind(sp_forestloss, sp_forestloss_new)
+  # iterate to next year
+  d = d+1
 }
 
-table(sp$year)
-plot(sp[sp$year == 1,])
-
-# TODO: convert numeric values to actual years?? ####
-
-
-## function to buffer around each road, only when x% of forest loss pixels in buffer, then road dev
-bufferRoads <- function(gfc_roads_pol, roads_buffer) {
-  # calculate area of buffers
+## function to check if x% of forest loss pixels lie in road buffer
+roadDev <- function(forestloss_pol, roads_buffer) {
+  # calculate area of road buffers
   roads_buffer$area <- area(roads_buffer)
   # calculate area of forest loss polygons
-  gfc_roads_pol$area <- area(gfc_roads_pol)
-  # count number of & calculate sum of area of forest loss pixels which lie in each buffer
-  pixelsInBuffer <- over(roads_buffer, gfc_roads_pol, fn = sum)
-  # get roads for which x% of forest loss pixels lie in buffer, then road dev
-  roads <- roads_buffer[((pixelsInBuffer$area / area(roads_buffer)) >= 0.05) & !is.na(pixelsInBuffer$area),]
+  forestloss_pol$area <- area(forestloss_pol)
+  # calculate area of forest loss polygons which lie in each road buffer
+  ForestLossOnRoad <- over(roads_buffer, forestloss_pol, fn = sum)
+  # get roads for which x% of forest loss polygons lie in buffer, then road dev
+  roads <- roads_buffer[((ForestLossOnRoad$area / area(roads_buffer)) >= 0.05) & !is.na(ForestLossOnRoad$area),]
   return(roads)
 }
 
 
-# 4. stepwise (cumulative) intersection of forest loss pixels & roads to find year of road development
-# a) intersect forest loss pixels of certain year with all roads
-# b) if >x% pixels intersecting with road - save year in road spatialpolygonsdataframe
-# c) else cumulatively add forest loss pixels to next year & repeat from a)
+## 4. stepwise (cumulative) intersection of forest loss polygons & roads to find year of road development
+## a) intersect forest loss pixels of certain year with all roads
+## b) if >x% pixels intersecting with road - save year in road spatialpolygonsdataframe
+## c) else cumulatively add forest loss pixels to next year & repeat from a)
 
 # index for years
 y <- 1
+
 # list for adding cumulative years
 year_list <- list(y)
 
 # add year variable to road polygons
 roads_union_buffer$year <- rep(NA, length(roads_union_buffer))
 
+# iterate over rasterstack layers (years)
 while(y < nlayers(gfc)) {
-  polygons_current_year <- sp[sp$year %in% year_list,]
-  # buffer around each road, only when x% of forest loss pixels in buffer, then road dev
-  roads <- bufferRoads(polygons_current_year, roads_union_buffer)
+  # get forest loss polygons of current year (from year list)
+  polygons_current_year <- sp_forestloss[sp_forestloss$year %in% year_list,]
+  # check if x% of forest loss polygons lie on road buffers, then road development
+  roads <- roadDev(polygons_current_year, roads_union_buffer)
   # if no road dev for that year, add year to list 
   if (length(roads) == 0) {
     year_list <- c(year_list, y)
   } else {
-  # check if not already year saved (to not overwrite earlier year)
+  # check if already year saved in year column (to not overwrite earlier year)
   if (is.na(mean(roads_union_buffer$year[roads_union_buffer@polygons %in% roads@polygons]))) {
-    # note year of intersecting roads
+    # if no year saved yet (na value), save year of intersecting roads
     roads_union_buffer$year[roads_union_buffer@polygons %in% roads@polygons] <- y
   }
   # reset list to current year
   year_list <- list(y)
   }
-  y=y+1
+  # iterate to next year
+  y = y + 1
 }
 
-# drop roads with na year
-roads_clean <- roads_union_buffer[!is.na(roads_union_buffer$year),]
+## remove roads with no corresponding forest loss between 2000 and 2020 (na in year column)
+roads_final <- roads_union_buffer[!is.na(roads_union_buffer$year),]
 
-lis <- seq(1:15)
-plot(sp[sp$year %in% lis,])
-#plot(roads,add=T)
-plot(roads_clean[roads_clean$year==16,], add=T, col="magenta")
+# lis <- seq(1:19)
+# plot(sp_forestloss[sp_forestloss$year %in% lis,])
+# plot(roads,add=T)
+# plot(roads_final[roads_final$year==19,], add=T, col="magenta")
 
-# roads not intersecting with forest loss pixels
-roads_na <- roads_union_buffer[is.na(roads_union_buffer$year),]
-plot(sp)
-plot(roads_na, col="green")
+## change year numbers to actual years
+roads_final$year <- as.numeric(roads_final$year)
+roads_final$year[roads_final$year <= 9] <- paste("200", roads_final$year[roads_final$year <= 9], sep = "")
+roads_final$year[!grepl("200", roads_final$year)] <- paste("20", roads_final$year[!grepl("200", roads_final$year)], sep = "")
 
 
-#### TODO: better & consistent naming ! ####
 
-# change numbers to actual years
-roads_clean$year <- as.numeric(roads_clean$year) #### TODO: directly specify as double when adding new column ####
-roads_clean$year[roads_clean$year <= 9] <- paste("200", roads_clean$year[roads_clean$year <= 9], sep="")
-roads_clean$year[!grepl("200", roads_clean$year)] <- paste("20", roads_clean$year[!grepl("200", roads_clean$year)], sep="")
+#### EXPORT OUTCOME ####
+
+## export polygons to shapefiles
+# export polygons of forest loss with corresponding years
+writeOGR(sp_forestloss, "GFC_ForestLossOnRoads_20002020.shp", driver = "ESRI Shapefile", dir)
+
+# export final roads with the corresponding years of there development
+writeOGR(roads_final, "RoadDevelopmentYears_20002020.shp", driver = "ESRI Shapefile", dir)
+
 
 
 #### VISUALIZATION ####
 
-# first convert data to sf
-roads_clean_sf <- st_as_sf(roads_clean)
+# first convert data to simple feature object (sf) (for easier plotting with ggplot)
+roads_final_sf <- st_as_sf(roads_final)
 
 # convert year to factor (for plotting)
-roads_clean_sf$year <- as.factor(roads_clean_sf$year)
+roads_final_sf$year <- as.factor(roads_final_sf$year)
 
-# basic plot
+## basic plot
 area <- 
-  ggplot(data=roads_clean_sf) +
+  ggplot(data=roads_final_sf) +
   geom_sf(aes(fill = year)) +
   #scale_fill_hue() +
   borders(aoi, colour = "gray85") +
   theme_map() 
-
+#### TODO: change color of lines ####
 plot(area)
 
-## Animation
 
-# animation - road development per year
+## animation of consecutive road development per year
 area +
   theme(legend.position = "none") +
   # Here comes the gganimate part
   transition_states(year) +
   transition_manual(year, cumulative = T) +
-  labs(title = "Road Development 2000-2020", subtitle = roads_clean_sf$year) + #"Year: {closest_state}"
+  labs(title = "Road Development 2000-2020", subtitle = roads_final_sf$year) + #"Year: {closest_state}" #### TODO:  adapt year to changing frame ####
   ease_aes('linear')
-
-## rather consecutive road development of previous years
-
